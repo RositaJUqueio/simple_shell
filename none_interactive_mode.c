32a560108b13539455a2235412fc0b447cc257d0
Rosita J uqueio <uqueiorose@gmai.com>
2023-04-27 17:10:34 -0700

    none interactive

---
+++ none_interactive_mode.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+
+/**
+ * non_interactive_mode - Executes the shell in non-interactive mode.
+ *
+ * Return: void
+ */
+void non_interactive_mode(void)
+{
+	char **current_command = NULL;
+       	int i, type_command = 0;
+	size_t n = 0;
+	
+	if (!(isatty(STDIN_FILENO)))
+	{
+        while (getline(&line, &n, stdin) != -1)
+        {
+            rm_newline(line);
+            rm_comment(line);
+            commands = tokenizer(line, ";");
+
+            for (i = 0; commands[i] != NULL; i++)
+            {
+                current_command = tokenizer(commands[i], " ");
+                if (current_command[0] == NULL)
+                {
+                    free(current_command);
+                    break;
+                }
+                type_command = parse_command(current_command[0]);
+                initializer(current_command, type_command);
+                free(current_command);
+            }
+            free(commands);
+        }
+        free(line);
+        exit(status);
+    }
+}
+
