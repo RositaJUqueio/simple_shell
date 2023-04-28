8774eede3e1e2f0c5a42c7ec0a4fb47c23c42ea6
Rosita J uqueio <uqueiorose@gmai.com>
2023-04-27 17:21:03 -0700

    betty styling

--- none_interactive_mode.c
+++ none_interactive_mode.c
@@ -8,33 +8,33 @@
 void non_interactive_mode(void)
 {
 	char **current_command = NULL;
-       	int i, type_command = 0;
+	int i, type_command = 0;
 	size_t n = 0;
-	
+
 	if (!(isatty(STDIN_FILENO)))
 	{
-        while (getline(&line, &n, stdin) != -1)
-        {
-            rm_newline(line);
-            rm_comment(line);
-            commands = tokenizer(line, ";");
+		while (getline(&line, &n, stdin) != -1)
+		{
+			rm_newline(line);
+			rm_comment(line);
+			commands = tokenizer(line, ";");
 
-            for (i = 0; commands[i] != NULL; i++)
-            {
-                current_command = tokenizer(commands[i], " ");
-                if (current_command[0] == NULL)
-                {
-                    free(current_command);
-                    break;
-                }
-                type_command = parse_command(current_command[0]);
-                initializer(current_command, type_command);
-                free(current_command);
-            }
-            free(commands);
-        }
-        free(line);
-        exit(status);
-    }
+			for (i = 0; commands[i] != NULL; i++)
+			{
+				current_command = tokenizer(commands[i], " ");
+				if (current_command[0] == NULL)
+				{
+					free(current_command);
+					break;
+				}
+				type_command = parse_command(current_command[0]);
+				initializer(current_command, type_command);
+				free(current_command);
+			}
+			free(commands);
+		}
+		free(line);
+		exit(status);
+	}
 }
 
