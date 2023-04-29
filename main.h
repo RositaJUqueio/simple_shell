7b9a5676a9d6af23b8fd76dd6068c001cb738f0a
Rosita Jose Uqueio <101433677+RositaJUqueio@users.noreply.github.com>
2023-04-29 12:57:57 +0200

    changes

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*global variables*/
+extern char **commands;
+extern char *line;
+extern char *shell_name;
+extern int status;
+
+
+#endif
---
+++ non_interactive_mode.c
@@ -0,0 +1,41 @@
+#include "shell.h"
+#include "main.h"
+
+/**
+ * none_interactive_mode - gets shell to operate
+ * in none interactive mode.
+ *
+ * Return: void
+ */
+void none_interactive_mode(void)
+{
+	char **current_command = NULL;
+	int i, type_command = 0;
+	size_t n = 0;
+
+	if (!(isatty(STDIN_FILENO)))
+	{
+		while (getline(&line, &n, stdin) != -1)
+		{
+			rm_newline(line);
+			rm_comment(line);
+			commands = tokenizer(line, ";");
+
+			for (i = 0; commands[i] != NULL; i++)
+			{
+				current_command = tokenizer(commands[i], " ");
+				if (current_command[0] == NULL)
+				{
+					free(current_command);
+					break;
+				}
+				type_command = parse_command(current_command[0]);
+				initializer(current_command, type_command);
+				free(current_command);
+			}
+			free(commands);
+		}
+		free(line);
+		exit(status);
+	}
+}
